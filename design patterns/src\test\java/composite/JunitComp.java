be784c208d520213bfcce36cfc6b11f0a0dea465
matthew <mematthew123@gmail.com>
2021-03-28 20:20:07 -0600

    updated

---
+++ design patterns/src\test\java/composite/JunitComp.java
@@ -0,0 +1,14 @@
+package composite;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JunitComp {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
